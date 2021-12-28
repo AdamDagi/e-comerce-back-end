d29936bdd50a33761953454711e7430152ab93ce
AdamDagi <mugutdinovshiha02@gmail.com>
2021-12-28 11:48:29 -0500

    starter code with created sql qode and readme

---
+++ Develop/db/schema.sql
@@ -0,0 +1,26 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+CREATE TABLE Category (    
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name TEXT NOT NULL
+);
+CREATE TABLE Product (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name TEXT NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT
+);
+CREATE TABLE Tag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name TEXT
+);
+CREATE TABLE ProductTag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT
+);
